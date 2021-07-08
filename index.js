268eb78999b650e144edf806f25ffeed6b6e6941
Joe Renteria <joe@electronflight.com>
2021-07-07 19:23:31 -0700

    card code added

--- index.js
+++ index.js
@@ -6,3 +6,28 @@ while (number >= 0)
 }
 
 countDown(10);
+
+
+
+
+
+------------------------------------
+
+
+
+
+
+
+const cards = [];
+
+function writeCards(guests,event){
+    for (let count = 0; count < guests.length; count++){
+        cards.unshift(`Thank you ${guests[count]}, for the wonderful ${event} gift...\n\n`)
+    }
+    
+}
+
+writeCards([`bill`,`bob`,`dave`,`jill`],`easter`);
+
+
+console.log(`${cards}`);
